fdb2ea38a81a295c7f09b32b4b6bd97b15f3a546
amiecollins <a_collins84448@fanshaweonline.ca>
2019-10-07 21:53:12 -0400

    sql working

--- app.js
+++ app.js
@@ -1,51 +1,74 @@
-const express = require("express");
-// must log in console "npm i -s express" to install package
-const hbs = require("hbs"); // aka "handlebars"
-// console command "npm i -s hbs"
-const path = require("path");
-// const sql = require("./utils/sql");
-const fs = require('fs');
-const partials = ["content", "contact", "jak", "burgermenu", "video"];
 
-const port = process.env.PORT || 3000;
+  const express = require("express");
+  // must log in console "npm i -s express" to install package
+  const hbs = require("hbs"); // aka "handlebars"
+  // console command "npm i -s hbs"
+  const path = require("path");
+  // const sql = require("./utils/sql");
+  const fs = require('fs');
 
-const app = express();
+  const sql = require("./utils/sql");
 
-app.use(express.static(__dirname + '/public'));
+  const port = process.env.PORT || 3336;
 
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname + "/views"));
+  const app = express();
 
-app.get("/", (req, res) => {
-  res.render("home", { homemessage: "hey there!", bio: "just anything" });
-})
+  var contentData = null;
 
-/*app.get("/users", (req, res) => {
+  var videoData = null;
 
-  sql.getConnection((err, connection) => {
-    if (err) {
-      return console.log(err.message);
-    }
-    let query = "SELECT * FROM tbl_card";
+  var locationData = null;
 
-    sql.query(query, (err, rows) => {
-      // we're done with our DB connection
-      connection.release();
-      // if something broke, quit and show error message
-      if (err) { return console.log(err.message); }
-      // show me the data!
-      console.log(rows);
 
-      res.render("user", rows[0]);
+
+  app.use(express.static(__dirname + '/public'));
+
+  app.set("view engine", "hbs");
+  app.set("views", path.join(__dirname + "/views"));
+
+  for (i = 0; i < 10 ; i++) {
+  app.get("/", (req, res) => {
+    
+      sql.getConnection((err, connection) => {
+    
+      if (err) {
+        return console.log(err.message);
+      }
+
+      let query = "SELECT * FROM video_tbl";
+
+      sql.query(query, (err, video) => {
+
+        if (err) { return console.log(err.message); }
+        videoData = video;
+        console.log(videoData);
+      })
+
+      query = "SELECT * FROM content_tbl";
+
+      sql.query(query, (err, content) => {
+
+        if (err) { return console.log(err.message); }
+        contentData = content;
+        console.log(contentData);
+      })
+
+      query = "SELECT * FROM locations_tbl";
+
+      sql.query(query, (err, location) => {
+        connection.release();
+        if (err) { return console.log(err.message); }
+        locationData = location;
+        console.log(locationData);
+      })
     })
+  
+  
+   res.render("home", { URL: videoData[0].URL });
   })
-});*/
+}
+  app.listen(port, () => {
+    console.log(`Server running at ${port}`);
+  });
 
-app.listen(port, () => {
-  console.log(`Server running at ${port}`);
-});
-var filePath = null;
-for (i = 0; i < partials.length; i++) { 
-    filePath = hbs.compile(fs.readFileSync(__dirname + `/views/${partials[i]}.hbs`).toString("utf-8"));
-    hbs.registerPartial(partials[i], filePath);
-};
+ 
